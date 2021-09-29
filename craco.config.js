1979bfcca949579da6e670f3c36b3f5c8bf35c28
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-09-30 00:42:56 +0700

    undo craco

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,11 @@
 // craco.config.js
-// const path = require(`path`);
-
-// const alias = (prefix = `src`) => ({
-// 	'@pages': `${prefix}/pages`,
-// 	'@components': `${prefix}/components`,
-// 	'@styles': `${prefix}/styles`,
-// 	'@utils': `${prefix}/utils`,
-// });
-
-// const SRC = `./src`;
-// const aliases = alias(SRC);
-
-// const resolvedAliases = Object.fromEntries(
-//   Object.entries(aliases).map(([key, value]) => [key, path.resolve(__dirname, value)]),
-// );
-
 module.exports = {
-	// webpack: {
-	// 	alias: resolvedAliases,
-	// },
 	style: {
-		postcss: {
+	postcss: {
 		plugins: [
-			require('tailwindcss'),
-			require('autoprefixer'),
+		require('tailwindcss'),
+		require('autoprefixer'),
 		],
-		},
+	},
 	},
 }
