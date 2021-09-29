bce36b7d8b062fe07fcca70901f1f1b25452bc0a
Rzkykhrllh <Rizkykirigaya@gmail.com>
2021-09-30 07:11:22 +0800

    test profile pages

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function profile() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen w-full bg-red-300">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default profile;
