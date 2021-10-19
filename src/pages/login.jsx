e7ecd297c220121f25f6a4834ef5fffc1955ffc7
GathN <gathot.h.nugroho@gmail.com>
2021-10-19 09:44:12 +0700

    login signup

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,38 +6,37 @@ import { Link } from 'react-router-dom'
 const Login = () => {
 	return (
 		<GuestOnlyRoute redirect="/dashboard">
-			<div className="absolute flex items-start flex-row justify-center relative p-20 w-screen h-screen">
-					<div className="relative w-3/12 flex flex-col items-start h-515px">
-						<h1 className="relative font-bold text-5xl text-black mt-8">
+			<div className="flex items-start flex-row justify-center relative p-20 w-screen h-screen">
+					<div className="w-3/12 h-515px flex flex-col items-start">
+						<h1 className="font-bold text-5xl text-black mt-8">
 							Login
 							</h1>
-						<h1 className="relative left-0 text-sm leading-5 mt-12 min-h-21px">
+						<h1 className="text-sm mt-12">
 							Email
 							</h1>
-						<input type="text" className="relative mt-2 h-42px w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Kontrakan@gmail.com">
+						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Email">
 							</input>
-						<h1 className="relative font-family-poppins left-0 text-sm leading-5 mt-4 min-h-21px">
+						<h1 className="text-sm mt-4">
 							Password
 							</h1>
-						<input type="password" className="relative mt-2 h-42px w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Kontrakan@gmail.com">
+						<input type="password" className="mt-2 h-42px w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Password">
 							</input>
-						<button type="button" className="mt-8 w-full h-10 text-white font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-400 rounded-lg">
+						<button type="button" className="mt-8 w-full h-10 transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl transform  text-white font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-400 rounded-lg">
 							Login
 							</button>
-						<div className="relative flex items-start flex-row justify-between mt-12 ">
+						<div className="flex items-start flex-row justify-between mt-8 full">
 							<div className="font-bold text-xs">
 								Don't have an account?
 								</div>
-							<button type="button" className="text-xs ml-20">
+							<button type="button" className="text-xs">
 								Forgot password?
 								</button>
 							</div>
-						 
-						 <Link to="/signup" className="w-full h-full">
-							 <button type="button" className="w-full mt-4 h-10 text-white font-bold tracking-wide bg-gradient-to-r from-gray-600 to-black rounded-lg">
-								 Register
-								</button>
-							</Link>	
+						<Link to="/signup" className="full"> 
+						<button type="button" className="w-full mt-6 h-10 transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl text-white font-bold tracking-wide bg-gradient-to-r from-gray-600 to-black rounded-lg">
+								<p>Register</p>
+						    </button>	
+						</Link>
 							
 						 <Link to="/dashboard" className="w-full h-full mt-6">Back</Link>	
 					</div>
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,10 +1,55 @@
 import React from "react";
 import GuestOnlyRoute from './../core/routeblocks/GuestOnlyRoute';
+import Splash from "./../images/Splash.png";
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return  (
 	<GuestOnlyRoute redirect="/dashboard">
-		ini signup
+		<div className="flex items-start flex-row justify-center relative p-10 w-screen h-screen">
+					<div className="w-3/12 flex flex-col items-start">
+						<h1 className="font-bold text-5xl text-black mt-4">
+							Register
+							</h1>
+						<h1 className="text-sm mt-8">
+							Email
+							</h1>
+						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Email">
+							</input>
+						<h1 className="text-sm mt-4">
+							Name
+							</h1>
+						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Nama">
+							</input>
+						<h1 className="text-sm mt-4">
+							Password
+							</h1>
+						<input type="password" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Password">
+							</input>
+						<h1 className="text-sm mt-4">
+							Confirm Password
+							</h1>
+						<input type="password" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Konfirmasi Password">
+							</input>
+						<button type="button" className="mt-8 w-full h-10 text-white font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-400 rounded-lg">
+							Register
+							</button>
+						<h1 className="font-bold text-xs mt-5 text-center full">
+							Already have an account?
+							</h1>
+						 
+						 <Link to="/login" className="w-full h-full">
+							 <button type="button" className="w-full mt-4 h-10 text-white font-bold tracking-wide bg-gradient-to-r from-gray-600 to-black rounded-lg">
+								 Login
+								</button>
+							</Link>	
+							
+						 <Link to="/dashboard">Back</Link>	
+					</div>
+					<div className="w-464px h-648px ml-20 mt-20">
+						<img src={Splash} alt="Splash"></img>
+				</div>
+			</div>
 	</GuestOnlyRoute>
   );
 }
