bdf236cc491df5985e316bfc0f2cacfcd119014c
M Fathur <mfathur17@gmail.com>
2021-10-21 09:33:30 +0700

    logic detail product completed

--- src/components/product/product_detail/ProductImages.jsx
+++ src/components/product/product_detail/ProductImages.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import pompom from '../../../images/pompom.png'
 
 const ProductImages = (props) => {
     return (
         <div>
-            <img src={props.images[0]} alt="Product image" className="bg-white shadow-xl" />
+            <img src={props.images[0]} alt="Product image" className="bg-white shadow-xl" style={{width:300,height:300}} />
             <div className="flex mt-3">
-                {/* <img src={pompom} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/>
-                <img src={pompom} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/>
-                <img src={pompom} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/> */}
+                {/* <img src={props.images[1]} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/>
+                <img src={props.images[2]} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/>
+                <img src={props.images[3]} alt="Product image" className="mr-2 bg-white shadow-md" style={{width:75,height:75}}/> */}
             </div>
         </div>
     );
--- src/components/product/product_detail/ProductOverview.jsx
+++ src/components/product/product_detail/ProductOverview.jsx
@@ -4,24 +4,40 @@ import { FaMinus, FaPlus } from 'react-icons/fa';
 const ProductOverview = (props) => {
   const [count, setCount] = useState(0);
   
+  const increment = ()=>{
+    setCount(count => count +1);
+  };
+
+  const decrement = () => {
+    setCount(count => {
+      const currentCount = count -1
+        if(currentCount<0){
+          return 0
+        } else {
+          return count-1
+        }
+      });
+  };
+  const currencyFormatter = Intl.NumberFormat('id-ID',{style:'currency',currency:'IDR'});
+
     return (
         <div >
             <h2 className="text-3xl font-semibold">{props.name}</h2>
             <div className="bg-gray-100 rounded-md">
               <h2 className="py-4 mt-6 mb-16 text-4xl font-semibold pl-7" style={{
                 background:"linear-gradient(90.6deg, #04ED49 0%, #00C5F2 100%)",
-                "-webkit-background-clip":"text","-webkit-text-fill-color":"transparent"}}>{props.price}</h2>  
+                "-webkit-background-clip":"text","-webkit-text-fill-color":"transparent"}}>{currencyFormatter.format(props.price)}</h2>  
             </div>
             
             <div className="flex">
-                <button
+                <button onClick={decrement}
                   className="p-4 bg-transparent border-2 border-gray-400 rounded-xl">
-                  <FaMinus size={42} className="text-xl text-gray-400 "/>
+                  <FaMinus size={42} className="text-xl text-gray-400"  />
                 </button>
                 <h2 className="mt-5 text-xl text-4xl font-semibold text-black mx-7">{count}</h2>
-                <button
+                <button onClick={increment}
                   className="p-4 mr-8 bg-transparent border-2 border-gray-400 rounded-xl" >
-                  <FaPlus size={42} className="text-xl text-gray-400 "/>
+                  <FaPlus size={42} className="text-xl text-gray-400 "  />
                 </button>
                 <button
                   className="py-4 text-4xl font-bold text-white rounded-xl px-14"
@@ -35,8 +51,7 @@ const ProductOverview = (props) => {
                     <h4 className="mb-1 text-xl text-gray-400">Available:</h4>
                     <h4 className="text-xl font-semibold text-gray-400">{props.amount_stock}</h4>
                 </div>
-            </div>
-            
+            </div>     
         </div>
     )
 }
