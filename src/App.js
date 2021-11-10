c050efa5dfce507f0399368e00f05394403952f2
stackoverprof <mail.errbint@gmail.com>
2021-11-10 22:49:13 +0700

    payment page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import ContextProvider from './core/contexts';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ContextProvider from "./core/contexts";
 
-import Home from './pages';
-import Login from './pages/login';
-import SignUp from './pages/signup';
-import SellerLogin from './pages/seller/login';
-import SellerSignUp from './pages/seller/signup';
-import Dashboard from './pages/dashboard';
-import Profile from './pages/profile';
+import Home from "./pages";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import SellerLogin from "./pages/seller/login";
+import SellerSignUp from "./pages/seller/signup";
+import Dashboard from "./pages/dashboard";
+import Profile from "./pages/profile";
 import ProductDetail from "./pages/product_detail";
-import Cart from "./pages/cart"
+import Cart from "./pages/cart";
 import Checkout from "./pages/checkout";
+import Payment from "./pages/payment";
 
 const App = () => {
-	return (
-		<ContextProvider>
-			<Router>
-				<Switch>
-					<Route exact path="/seller/login" component={SellerLogin} />
-					<Route exact path="/seller/signup" component={SellerSignUp} />
-					<Route exact path="/" component={Home} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/signup" component={SignUp} />
-					<Route exact path="/dashboard" component={Dashboard} />
-					<Route exact path="/profile" component={Profile} />
-					<Route exact path="/product/:id" component={ProductDetail} />
-					<Route exact path="/cart" component={Cart} />
-					<Route exact path="/checkout" component={Checkout} />
-				</Switch>
-			</Router>
-		</ContextProvider>
-	);
-}
+  return (
+    <ContextProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/seller/login" component={SellerLogin} />
+          <Route exact path="/seller/signup" component={SellerSignUp} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/product/:id" component={ProductDetail} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/payment" component={Payment} />
+        </Switch>
+      </Router>
+    </ContextProvider>
+  );
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,49 +1,50 @@
 module.exports = {
-	mode: 'jit',
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		container: {
-			padding: {
-				DEFAULT: '5%',
-				sm: '32px',
-			},
-			center: true
-		},
-		extend: {
-			colors: {
-				base: '#ffffff',
-				primary: {
-					DEFAULT: '#3b3b3b',
-					red: '#FC3C5B'
-				}
-			},
-			fontFamily: {
-				main: 'Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-				slab: 'Patua One, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-			},
-			screens: {
-				'-2xl': { raw: '(max-width: 1535px)' },
-				'-xl': { raw: '(max-width: 1279px)' },
-				'-lg': { raw: '(max-width: 1023px)' },
-				'-md': { raw: '(max-width: 767px)' },
-				'-sm': { raw: '(max-width: 639px)' },
-			},
-			boxShadow: {
-				sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-				DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-				md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-				lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-				xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-				'2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-				inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-				none: 'none',
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "5%",
+        sm: "32px",
+      },
+      center: true,
+    },
+    extend: {
+      colors: {
+        base: "#ffffff",
+        primary: {
+          DEFAULT: "#3b3b3b",
+          red: "#FC3C5B",
+        },
+      },
+      fontFamily: {
+        main: "Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+        slab: "Patua One, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+      },
+      screens: {
+        "-2xl": { raw: "(max-width: 1535px)" },
+        "-xl": { raw: "(max-width: 1279px)" },
+        "-lg": { raw: "(max-width: 1023px)" },
+        "-md": { raw: "(max-width: 767px)" },
+        "-sm": { raw: "(max-width: 639px)" },
+      },
+      boxShadow: {
+        sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+        DEFAULT:
+          "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+        md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+        lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+        xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+        "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+        inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+        none: "none",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
