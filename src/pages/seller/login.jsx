febae946be32de6a1a35cee4070030f045fba285
stackoverprof <mail.errbint@gmail.com>
2021-12-02 05:35:45 +0700

    feat: login signup seller

--- src/pages/seller/login.jsx
+++ src/pages/seller/login.jsx
@@ -1,51 +1,88 @@
 import React from "react";
 import GuestOnlyRoute from "src/core/routeblocks/GuestOnlyRoute";
 import Splash2 from "src/images/Splash2.png";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+
+import { useAuth } from "src/core/contexts";
+import useForm from "src/core/hooks/useForm";
 
 const Login = () => {
-	return (
-		<GuestOnlyRoute redirect="/dashboard">
-			<div className="flex items-start flex-row justify-center relative p-20 w-screen h-screen">
-					<div className="w-3/12 h-515px flex flex-col items-start">
-						<h1 className="font-bold text-5xl text-black mt-8">
-							Login
-							</h1>
-						<h1 className="text-sm mt-12">
-							Email
-							</h1>
-						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Email">
-							</input>
-						<h1 className="text-sm mt-4">
-							Password
-							</h1>
-						<input type="password" className="mt-2 h-42px w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Password">
-							</input>
-						<button type="button" className="mt-8 w-full h-10 transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl transform  text-white font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-400 rounded-lg">
-							Login
-							</button>
-						<div className="flex items-start flex-row justify-between mt-8 full">
-							<div className="font-bold text-xs">
-								Don't have an account?
-								</div>
-							<button type="button" className="text-xs">
-								Forgot password?
-								</button>
-							</div>
-						<Link to="/seller/signup" className="full"> 
-						<button type="button" className="w-full mt-6 h-10 transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl text-white font-bold tracking-wide bg-gradient-to-r from-gray-600 to-black rounded-lg">
-								<p>Register</p>
-						    </button>	
-						</Link>
-							
-						 <Link to="/dashboard" className="w-full h-full mt-6">Back</Link>	
-					</div>
-					<Link to="/login" className="relative w-464px h-532px ml-20 flex flex-col rounded-lg">
-						<img src={Splash2} alt="Splash2" className="w-full h-90px relative ml-38px mt-38px transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl"></img>
-				</Link>
-			</div>
-		</GuestOnlyRoute>
-	);
-}
+  const { authMethods } = useAuth();
+  const { form, mutateForm, resetForm } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async () => {
+    const result = await authMethods.login({
+      email: form.email,
+      password: form.password,
+    });
+    console.log(result);
+  };
+
+  return (
+    <GuestOnlyRoute redirect="/dashboard">
+      <div className="relative flex flex-row justify-center items-start p-20 w-screen h-screen">
+        <div className="flex flex-col items-start w-3/12 h-515px">
+          <h1 className="mt-8 text-5xl font-bold text-black">Login</h1>
+          <h1 className="mt-12 text-sm">Email</h1>
+          <input
+            value={form.email}
+            onChange={mutateForm}
+            name="email"
+            type="text"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black"
+            placeholder="Masukkan Email"
+          ></input>
+          <h1 className="mt-4 text-sm">Password</h1>
+          <input
+            value={form.password}
+            onChange={mutateForm}
+            name="password"
+            type="password"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black h-42px"
+            placeholder="Masukkan Password"
+          ></input>
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className="mt-8 w-full h-10 font-bold tracking-wide text-white bg-gradient-to-r from-green-400 to-blue-400 rounded-lg transition duration-700 ease-in-out transform hover:-translate-y-1 hover:shadow-xl"
+          >
+            Login
+          </button>
+          <div className="flex flex-row justify-between items-start mt-8 full">
+            <div className="text-xs font-bold">Don't have an account?</div>
+            <button type="button" className="text-xs">
+              Forgot password?
+            </button>
+          </div>
+          <Link to="/seller/signup" className="full">
+            <button
+              type="button"
+              className="mt-6 w-full h-10 font-bold tracking-wide text-white bg-gradient-to-r from-gray-600 to-black rounded-lg transition duration-700 ease-in-out hover:-translate-y-1 hover:shadow-xl"
+            >
+              <p>Register</p>
+            </button>
+          </Link>
+
+          <Link to="/dashboard" className="mt-6 w-full h-full">
+            Back
+          </Link>
+        </div>
+        <Link
+          to="/login"
+          className="relative flex flex-col ml-20 rounded-lg w-464px h-532px"
+        >
+          <img
+            src={Splash2}
+            alt="Splash2"
+            className="relative w-full transition duration-700 ease-in-out h-90px ml-38px mt-38px hover:-translate-y-1 hover:shadow-xl"
+          ></img>
+        </Link>
+      </div>
+    </GuestOnlyRoute>
+  );
+};
 
 export default Login;
--- src/pages/seller/signup.jsx
+++ src/pages/seller/signup.jsx
@@ -1,61 +1,105 @@
 import React from "react";
 import GuestOnlyRoute from "src/core/routeblocks/GuestOnlyRoute";
 import Splash2 from "src/images/Splash2.png";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useAuth } from "src/core/contexts";
+import useForm from "src/core/hooks/useForm";
 
 const SignUp = () => {
-  return  (
-	<GuestOnlyRoute redirect="/dashboard">
-		<div className="flex items-start flex-row justify-center relative p-10 w-screen h-screen">
-					<div className="w-3/12 flex flex-col items-start">
-						<h1 className="font-bold text-5xl text-black mt-4">
-							Register
-							</h1>
-						<h1 className="text-sm mt-8">
-							Email
-							</h1>
-						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Email">
-							</input>
-						<h1 className="text-sm mt-4">
-							Name
-							</h1>
-						<input type="text" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Nama">
-							</input>
-						<h1 className="text-sm mt-4">
-							Password
-							</h1>
-						<input type="password" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Masukkan Password">
-							</input>
-						<h1 className="text-sm mt-4">
-							Confirm Password
-							</h1>
-						<input type="password" className="mt-2 w-full border-2 border-black rounded-lg p-2 pl-3" placeholder="Konfirmasi Password">
-							</input>
-						<button type="button" className="mt-8 w-full h-10 text-white font-bold tracking-wide bg-gradient-to-r from-green-400 to-blue-400 rounded-lg transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl">
-							Register
-							</button>
-						<h1 className="font-bold text-xs mt-5 text-center full">
-							Already have an account?
-							</h1>
-						 
-						 <Link to="/seller/login" className="w-full h-full">
-							 <button type="button" className="w-full mt-4 h-10 text-white font-bold tracking-wide bg-gradient-to-r from-gray-600 to-black rounded-lg transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl">
-								 Login
-								</button>
-							</Link>	
-							
-						 <Link to="/dashboard">Back</Link>	
-					</div>
-					
-					<Link to="/signup" className="w-464px h-648px ml-20 mt-20">
-					<div className="transition ease-in-out duration-700 hover:-translate-y-1 hover:shadow-xl">
-						<img src={Splash2} alt="Splash2" ></img>
-						
-					</div>
-					</Link>
-			</div>
-	</GuestOnlyRoute>
+  const { authMethods } = useAuth();
+  const { form, mutateForm, resetForm } = useForm({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = async () => {
+    if (form.password !== form.confirmPassword) {
+      console.log("nggak sama");
+      return;
+    }
+
+    const result = await authMethods.signup({
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    });
+    console.log(result);
+  };
+
+  return (
+    <GuestOnlyRoute redirect="/dashboard">
+      <div className="relative flex flex-row justify-center items-start p-10 w-screen h-screen">
+        <div className="flex flex-col items-start w-3/12">
+          <h1 className="mt-4 text-5xl font-bold text-black">Register</h1>
+          <h1 className="mt-8 text-sm">Email</h1>
+          <input
+            value={form.email}
+            onChange={mutateForm}
+            name="email"
+            type="text"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black"
+            placeholder="Masukkan Email"
+          ></input>
+          <h1 className="mt-4 text-sm">Name</h1>
+          <input
+            value={form.name}
+            onChange={mutateForm}
+            name="name"
+            type="text"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black"
+            placeholder="Masukkan Nama"
+          ></input>
+          <h1 className="mt-4 text-sm">Password</h1>
+          <input
+            value={form.password}
+            onChange={mutateForm}
+            name="password"
+            type="password"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black"
+            placeholder="Masukkan Password"
+          ></input>
+          <h1 className="mt-4 text-sm">Confirm Password</h1>
+          <input
+            value={form.confirmPassword}
+            onChange={mutateForm}
+            name="confirmPassword"
+            type="password"
+            className="p-2 pl-3 mt-2 w-full rounded-lg border-2 border-black"
+            placeholder="Konfirmasi Password"
+          ></input>
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className="mt-8 w-full h-10 font-bold tracking-wide text-white bg-gradient-to-r from-green-400 to-blue-400 rounded-lg transition duration-700 ease-in-out hover:-translate-y-1 hover:shadow-xl"
+          >
+            Register
+          </button>
+          <h1 className="mt-5 text-xs font-bold text-center full">
+            Already have an account?
+          </h1>
+
+          <Link to="/seller/login" className="w-full h-full">
+            <button
+              type="button"
+              className="mt-4 w-full h-10 font-bold tracking-wide text-white bg-gradient-to-r from-gray-600 to-black rounded-lg transition duration-700 ease-in-out hover:-translate-y-1 hover:shadow-xl"
+            >
+              Login
+            </button>
+          </Link>
+
+          <Link to="/dashboard">Back</Link>
+        </div>
+
+        <Link to="/signup" className="mt-20 ml-20 w-464px h-648px">
+          <div className="transition duration-700 ease-in-out hover:-translate-y-1 hover:shadow-xl">
+            <img src={Splash2} alt="Splash2"></img>
+          </div>
+        </Link>
+      </div>
+    </GuestOnlyRoute>
   );
-}
+};
 
 export default SignUp;
